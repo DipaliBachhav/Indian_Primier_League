7c60f48795f25774e449efc123e51839d526aa89
DipaliBachhav <dipa2718@gmail.com>
2020-05-20 09:54:22 +0530

    Added Code to Load Empty Data Of Batmans Information from CSV File

--- src/test/java/com/CricketData/CricketAnalyzerTest.java
+++ src/test/java/com/CricketData/CricketAnalyzerTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 public class CricketAnalyzerTest {
     private static final String BATSMAN_DATA = "C:\\Users\\Windows 10\\Desktop\\Indian_Primier_League\\src\\test\\resources\\IPL2019FactsheetMostRuns.csv";
     private static final String BATSMAN_DATA_INCORRECT_FILE = "C:\\Users\\Windows 10\\Desktop\\Indian_Primier_League\\src\\test\\resources\\IPL2019FactsheetMostWkts";
+    private static final String BATSMAN_DATA_EMPTY_FILE = "C:\\Users\\Windows 10\\Desktop\\Indian_Primier_League\\src\\test\\resources\\IPL2019FactsheetMostRunsEmpty";
 
 
     @Test
@@ -24,6 +25,16 @@ public class CricketAnalyzerTest {
             CricketAnalyzer cricketLeagueAnalysis = new CricketAnalyzer();
             int numOfRecords = cricketLeagueAnalysis.loadCricketLeagueData(BATSMAN_DATA_INCORRECT_FILE);
             Assert.assertEquals(100, numOfRecords);
+        } catch (CricketAnalyzerException e) {
+        }
+    }
+
+    @Test
+    public void givenIPLBatsmanData_shouldReturnEmptyCSVFile() {
+        try {
+            CricketAnalyzer cricketLeagueAnalysis = new CricketAnalyzer();
+            int numOfRecords = cricketLeagueAnalysis.loadCricketLeagueData(BATSMAN_DATA_EMPTY_FILE);
+            Assert.assertEquals(0, numOfRecords);
         } catch (CricketAnalyzerException e) {
         }
     }
